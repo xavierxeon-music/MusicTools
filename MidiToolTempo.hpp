0a50c1ec29041a6084dd04ece75b002757995143
Ralf Waspe <rwaspe@icloud.com>
2022-04-04 23:20:01 +0200

    removed note on

--- MidiToolTempo.hpp
+++ MidiToolTempo.hpp
@@ -6,23 +6,18 @@
 Midi::Tool::Tempo::Tempo(Interface::Input* input)
    : tickCounter(0)
 {
-   input->onNoteOn(this, &Tempo::noteOn);
    input->onClockTick(this, &Tempo::clockTick);
    input->onClockStatus(this, &Tempo::clockStatus);
 }
 
-void Midi::Tool::Tempo::noteOn(const Channel& channel, const Note& note, const Velocity& velocity)
-{
-   Q_UNUSED(note)
-   Q_UNUSED(velocity)
-
-   qDebug() << tickCounter << channel;
-   tickCounter = 0;
-}
 
 void Midi::Tool::Tempo::clockTick()
 {
    tickCounter++;
+   if (6 == tickCounter)
+   {
+      tickCounter = 0;
+   }
 }
 
 void Midi::Tool::Tempo::clockStatus(const Playback& status)
