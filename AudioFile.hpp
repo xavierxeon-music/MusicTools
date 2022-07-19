4fe25646f60320ee9afde6b400eef66e7155571a
Ralf Waspe <raw@mmmi.sdu.dk>
2022-07-19 15:01:05 +0200

    wav file header

--- AudioFile.hpp
+++ AudioFile.hpp
@@ -5,9 +5,31 @@
 
 #include <iostream>
 
+static const uint16_t BUFFER_SIZE = 4096;
+
+// header
+
+AudioFile::Header::Header()
+{
+   Subchunk1Size = 16; // default for PCM
+   audioFormat = 1;
+   numChannels = 2;
+   sampleRate = 48000;
+   bitsPerSample = 32;
+
+   bytesPerSec = sampleRate * numChannels * bitsPerSample / 8;
+   blockAlign = numChannels * bitsPerSample / 8;
+
+   chunkSize = 0;     // RIFF Chunk Size
+   subchunk2Size = 0; // Sampled data length
+}
+
+// audio file
+
 AudioFile::AudioFile()
-   : header{}
-   , content{}
+   : sampleRate(48000)
+   , leftData()
+   , rightData()
 {
 }
 
@@ -20,28 +42,48 @@ bool AudioFile::load(const std::string& fileName)
       return false;
    }
 
+   Header header;
    size_t bytesRead = fread(&header, 1, sizeof(Header), wavFile);
    std::cout << "Header Read " << bytesRead << " bytes." << std::endl;
 
-   uint16_t bytesPerSample = header.bitsPerSample / 8;      //Number     of bytes per sample
-   uint64_t numSamples = header.ChunkSize / bytesPerSample; //How many samples are in the wav file?
+   if (bytesRead <= 0)
+      return false;
 
    std::cout << "RIFF header                :" << header.RIFF[0] << header.RIFF[1] << header.RIFF[2] << header.RIFF[3] << std::endl;
    std::cout << "WAVE header                :" << header.WAVE[0] << header.WAVE[1] << header.WAVE[2] << header.WAVE[3] << std::endl;
    std::cout << "FMT                        :" << header.fmt[0] << header.fmt[1] << header.fmt[2] << header.fmt[3] << std::endl;
-   std::cout << "Data size                  :" << header.ChunkSize << std::endl;
+   std::cout << "Data size                  :" << header.chunkSize << std::endl;
 
    // Display the sampling Rate from the header
-   std::cout << "Sampling Rate              :" << header.SamplesPerSec << std::endl;
+   std::cout << "Sampling Rate              :" << header.sampleRate << std::endl;
    std::cout << "Number of bits used        :" << header.bitsPerSample << std::endl;
-   std::cout << "Number of channels         :" << header.NumOfChan << std::endl;
+   std::cout << "Number of channels         :" << header.numChannels << std::endl;
    std::cout << "Number of bytes per second :" << header.bytesPerSec << std::endl;
-   std::cout << "Data length                :" << header.Subchunk2Size << std::endl;
-   std::cout << "Audio Format               :" << header.AudioFormat << std::endl;
+   std::cout << "Data length                :" << header.subchunk2Size << std::endl;
+   std::cout << "Audio Format               :" << header.audioFormat << std::endl;
    // Audio format 1=PCM,6=mulaw,7=alaw, 257=IBM Mu-Law, 258=IBM A-Law, 259=ADPCM
 
    std::cout << "Block align                :" << header.blockAlign << std::endl;
-   std::cout << "Data string                :" << header.Subchunk2ID[0] << header.Subchunk2ID[1] << header.Subchunk2ID[2] << header.Subchunk2ID[3] << std::endl;
+   std::cout << "Data string                :" << header.subchunk2ID[0] << header.subchunk2ID[1] << header.subchunk2ID[2] << header.subchunk2ID[3] << std::endl;
+
+   leftData.clear();
+   rightData.clear();
+
+   uint16_t bytesPerSample = header.bitsPerSample / 8;      //Number     of bytes per sample
+   uint64_t numSamples = header.chunkSize / bytesPerSample; //How many samples are in the wav file?
+
+   int8_t buffer[BUFFER_SIZE];
+   while (true)
+   {
+      bytesRead = fread(buffer, sizeof(buffer[0]), BUFFER_SIZE / (sizeof buffer[0]), wavFile);
+      if (bytesRead <= 0)
+         break;
+
+      /** DO SOMETHING WITH THE WAVE DATA HERE **/
+      std::cout << "Read " << bytesRead << " bytes." << std::endl;
+   }
+
+   fclose(wavFile);
 
    return true;
 }
--- Include/Sound/AudioFile.h
+++ Include/Sound/AudioFile.h
@@ -3,47 +3,45 @@
 
 #include <MusicTools.h>
 
+// see http://soundfile.sapp.org/doc/WaveFormat/
+
+// pcm wav files
 class AudioFile
 {
-public:
-   struct Header
-   {
-      // RIFF Chunk Descriptor
-      uint8_t RIFF[4];    // RIFF Header Magic header
-      uint32_t ChunkSize; // RIFF Chunk Size
-      uint8_t WAVE[4];    // WAVE Header
-      // "fmt" sub-chunk
-      uint8_t fmt[4];           // FMT header
-      uint32_t Subchunk1Size;   // Size of the fmt chunk
-      uint16_t AudioFormat = 1; // Audio format 1=PCM,6=mulaw,7=alaw,     257=IBM Mu-Law, 258=IBM A-Law, 259=ADPCM
-      uint16_t NumOfChan;       // Number of channels 1=Mono 2=Sterio
-      uint32_t SamplesPerSec;   // Sampling Frequency in Hz
-      uint32_t bytesPerSec;     // bytes per second
-      uint16_t blockAlign;      // 2=16-bit mono, 4=16-bit stereo
-      uint16_t bitsPerSample;   // Number of bits per sample
-      // "data" sub-chunk
-      uint8_t Subchunk2ID[4]; // "data"  string
-      uint32_t Subchunk2Size; // Sampled data length
-   };
-
-   struct Content
-   {
-      std::vector<float> leftData;
-      std::vector<float> rightData;
-   };
-
 public:
    inline AudioFile();
 
 public:
    inline bool load(const std::string& fileName);
-
-   // TODO: set sample rate
    inline bool save(const std::string& fileName);
 
+public:
+   uint16_t sampleRate;
+   std::vector<float> leftData;
+   std::vector<float> rightData;
+
 private:
-   Header header;
-   Content content;
+   struct Header
+   {
+      // RIFF Chunk Descriptor
+      uint8_t RIFF[4] = {'R', 'I', 'F', 'F'}; // RIFF Header Magic header
+      uint32_t chunkSize;                     // RIFF Chunk Size
+      uint8_t WAVE[4] = {'W', 'A', 'V', 'E'}; // WAVE Header
+      // "fmt" sub-chunk
+      uint8_t fmt[4] = {'f', 'm', 't', ' '}; // FMT header
+      uint32_t Subchunk1Size;                // Size of the fmt chunk
+      uint16_t audioFormat;                  // Audio format 1=PCM,6=mulaw,7=alaw, 257=IBM Mu-Law, 258=IBM A-Law, 259=ADPCM
+      uint16_t numChannels;                  // Number of channels 1=Mono 2=Stereo
+      uint32_t sampleRate;                   // Sampling Frequency in Hz
+      uint32_t bytesPerSec;                  // bytes per second
+      uint16_t blockAlign;                   // 2=16-bit mono, 4=16-bit stereo
+      uint16_t bitsPerSample;                // Number of bits per sample
+      // "data" sub-chunk
+      uint8_t subchunk2ID[4] = {'d', 'a', 't', 'a'}; // "data"  string
+      uint32_t subchunk2Size;                        // Sampled data length
+
+      Header();
+   };
 };
 
 #ifndef AudioFileHPP
