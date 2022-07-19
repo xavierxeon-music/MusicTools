0d75f583e07ee4b411323a826df32afd23fa2713
Ralf Waspe <raw@mmmi.sdu.dk>
2022-07-19 08:55:19 +0200

    added wave file

---
+++ Include/Sound/WaveFile.h
@@ -0,0 +1,14 @@
+#ifndef WaveFileH
+#define WaveFileH
+
+class WaveFile
+{
+public:
+   inline WaveFile();
+};
+
+#ifndef WaveFileHPP
+#include "WaveFile.hpp"
+#endif // NOT WaveFileHPP
+
+#endif // NOT WaveFileH
---
+++ WaveFile.hpp
@@ -0,0 +1,10 @@
+#ifndef WaveFileHPP
+#define WaveFileHPP
+
+#include "WaveFile.h"
+
+WaveFile::WaveFile()
+{
+}
+
+#endif // NOT WaveFileHPP
