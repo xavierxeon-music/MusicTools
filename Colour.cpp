2030fc747a1a2157d57e15327a6ad9bcfd60d801
Ralf Waspe <rwaspe@icloud.com>
2022-09-30 23:08:16 +0200

    color

--- Colour.cpp
+++ Colour.cpp
@@ -1,14 +1,14 @@
 #include <Tools/Colour.h>
 
-const Color::NoteMap Color::noteMap = {{Note::C, Color{255, 255, 255}},
-                                       {Note::Cs, Color{90, 90, 90}},
-                                       {Note::D, Color{255, 0, 0}},
-                                       {Note::Ds, Color{90, 0, 0}},
-                                       {Note::E, Color{255, 30, 200}},
-                                       {Note::F, Color{0, 0, 255}},
-                                       {Note::Fs, Color{0, 0, 90}},
-                                       {Note::G, Color{0, 255, 0}},
-                                       {Note::Gs, Color{0, 90, 0}},
-                                       {Note::A, Color{255, 255, 0}},
-                                       {Note::As, Color{90, 90, 0}},
-                                       {Note::B, Color{0, 0, 0}}};
+const Colour::NoteMap Colour::noteMap = {{Note::C, Colour{255, 255, 255}},
+                                         {Note::Cs, Colour{90, 90, 90}},
+                                         {Note::D, Colour{255, 0, 0}},
+                                         {Note::Ds, Colour{90, 0, 0}},
+                                         {Note::E, Colour{255, 30, 200}},
+                                         {Note::F, Colour{0, 0, 255}},
+                                         {Note::Fs, Colour{0, 0, 90}},
+                                         {Note::G, Colour{0, 255, 0}},
+                                         {Note::Gs, Colour{0, 90, 0}},
+                                         {Note::A, Colour{255, 255, 0}},
+                                         {Note::As, Colour{90, 90, 0}},
+                                         {Note::B, Colour{0, 0, 0}}};
--- Include/Tools/Colour.h
+++ Include/Tools/Colour.h
@@ -6,13 +6,13 @@
 #include <Music/Note.h>
 #include <MusicTools.h>
 
-struct Color
+struct Colour
 {
    uint8_t red;
    uint8_t green;
    uint8_t blue;
 
-   using NoteMap = std::map<Note::Value, Color>;
+   using NoteMap = std::map<Note::Value, Colour>;
    static const NoteMap noteMap;
 };
 
